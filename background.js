5eb37126fc961e01ec2ee427e93a9827c0ef00a9
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-04-20 17:00:05 +0900

    📝 Html수정 및 로그 추가

--- background.js
+++ background.js
@@ -15,8 +15,11 @@ chrome.tabs.onRemoved.addListener(function(tabId) {
 
 // 컨텍스트 메뉴 클릭 이벤트 리스너
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    console.log("selected!")
     if (info.menuItemId === "copyText" && info.selectionText) {
+        console.log("Ask! Clicked")
         if (tab.id && !injectedTabs[tab.id]) {
+            console.log("script injected!")
             // content.js가 아직 삽입되지 않았다면 삽입 => 중복 삽입으로 인한 중복 호출 방지
             chrome.scripting.executeScript({
                 target: {tabId: tab.id},
@@ -31,6 +34,7 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
                 }
             });
         } else {
+            console.log("script currently injected")
             sendMessageToContentScript(tab, info); // 이미 삽입되었다면 바로 메시지를 보냄
         }
     }
@@ -38,6 +42,7 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
 
 
 function sendMessageToContentScript(tab, info) {
+    console.log("called sendMessageToContentScript")
     chrome.tabs.sendMessage(tab.id, { text: info.selectionText }, function(response) {
         if (chrome.runtime.lastError) {
             console.error("Error sending message:", chrome.runtime.lastError.message);
@@ -46,3 +51,10 @@ function sendMessageToContentScript(tab, info) {
         }
     });
 }
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // 탭의 URL이 변경되었거나 페이지가 새로고침된 경우
+    if (changeInfo.url || changeInfo.status === "complete") {
+        delete injectedTabs[tabId];  // 삽입 상태 초기화
+    }
+});
